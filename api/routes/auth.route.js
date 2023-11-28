9f101fd0ec33cdb6bd76b582977deb1460dbc0e0
no-humans <pranavprasad242@gmail.com>
2023-11-28 13:00:07 +0530

    sign in api route (jwt) created

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js'
+import { siginin, signup } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
 router.post("/signup",signup) 
+
+router.post("/signin",siginin) 
 
 export default router
