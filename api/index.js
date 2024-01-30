f9e063281d695a0f17dffcca2835efd31b62c771
no-humans <pranavprasad242@gmail.com>
2024-01-30 12:58:45 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from './routes/listing.route.js'
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // middleware
 
